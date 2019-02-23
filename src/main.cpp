fbd860de5e050d526d0cb055df2d7a967a5b2694
Orest-ForBe <47846738+Orest-ForBe@users.noreply.github.com>
2019-02-23 15:01:34 +0200

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 23 лют. 2019 р.
+ *      Author: ForBe
+ */
+#include <windows.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
+#if defined(_WIN32)
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#else
+int main(int argc, char **argv)
+#endif
+{
+
+    return 0;
+}
+
+
+
